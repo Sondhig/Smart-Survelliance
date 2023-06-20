b662920de5f69fa15f1af3163051da6d64310073
Sondhig <94097282+Sondhig@users.noreply.github.com>
2023-06-20 08:10:29 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,196 @@
+# Flask modules
+from flask import Flask, render_template, Response, request, flash, url_for, redirect, get_flashed_messages, session, send_file,send_from_directory
+
+# OpenCV
+import cv2
+import numpy as np
+
+# Basic Python modules
+from PIL import Image
+import os
+import sys
+import csv
+import shutil
+import zipfile
+
+# SSH Connection
+import paramiko
+
+# List of important directories
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, '/backend')
+yml_dir = os.path.join(BASE_DIR, "backend/trainer.yml")
+pickle_dir = os.path.join(BASE_DIR, "backend/labels.pickle")
+images_dir = os.path.join(BASE_DIR, "backend/images")
+csv_dir = os.path.join(BASE_DIR, "backend/capture_history.csv")
+videos_dir = os.path.join(BASE_DIR, "backend/videos")
+
+# Hand-made functions
+from backend import generate
+from backend import Live_Feed
+from backend import capture
+from backend import trainer
+
+from hardware import SSH_left
+from hardware import SSH_right
+
+# Init Flask Web App and Secret Key to store sessions
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+
+# Create Home route
+@app.route('/', methods=["POST", "GET"])
+def index():
+    messages = get_flashed_messages()
+    hst = session.get('hst')
+    usr = session.get('usr')
+    pwd = session.get('pwd')
+    email = session.get('email')
+    email_pwd = session.get('email_pwd')
+    return render_template('home.html', messages=messages, hst=hst, usr=usr, pwd=pwd, email=email, email_pwd=email_pwd)
+
+# Create stationary camera interface
+@app.route('/video', methods=["POST", "GET"])
+def video():
+    email = session.get('email')
+    email_pwd = session.get('email_pwd')
+    return Response(generate.generate_frames(0, images_dir, videos_dir, email, email_pwd), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+# Create moving camera interface
+@app.route('/moving', methods=["POST", "GET"])
+def moving():
+    email = session.get('email')
+    email_pwd = session.get('email_pwd')
+    return Response(generate.generate_frames(1, images_dir, videos_dir, email, email_pwd), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+# Create Add route to capture training data
+@app.route('/add', methods=["POST", "GET"])
+def add():
+    if request.method == "POST":
+        name = request.form['name']
+        n = request.form['num']
+        capture.capture(name, n)
+        return render_template('train.html', name=name)
+    else:
+        return render_template('add.html')
+
+# Add live route to display live feed in Add
+@app.route('/live')
+def live():
+    return Response(Live_Feed.generateFeed(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+# Create Train route to train model and store new information in YML file
+@app.route('/train', methods=["POST", "GET"])
+def train():
+    if request.method == "POST":
+        trainer.trainer(yml_dir, pickle_dir)
+        flash('Training completed successfully!', 'success')
+        return redirect(url_for('index'))
+    return render_template('train.html')
+
+# Create Settings route to set machine and email parameters
+@app.route('/settings', methods=["POST", "GET"])
+def settings():
+    if request.method == "POST":
+        # retrieve the form data submitted by the user
+        hst = request.form['hst']
+        usr = request.form['usr']
+        pwd = request.form['pwd']
+        email = request.form['email']
+        email_pwd = request.form['email_pwd']
+        # store the values in the session
+        session['hst'] = hst
+        session['usr'] = usr
+        session['pwd'] = pwd
+        session['email'] = email
+        session['email_pwd'] = email_pwd
+        return redirect(url_for('index'))
+    return render_template('settings.html')
+
+# Create Left route for turning the camera left
+@app.route('/left', methods=["POST", "GET"])
+def left():
+    if request.method =="POST":
+        print("LEFT REQUEST IS SUCCESFUL")
+        # os.system("python SSH_left.py")
+        # retrieve the parameter values from session
+        hst = session.get('hst', '10.0.0.83')
+        usr = session.get('usr', 'gurkaran')
+        pwd = session.get('pwd', 'gurkaran')
+        # call connectLeft with the retrieved values
+        SSH_left.connectLeft(hst, usr, pwd)
+        return ('', 204)
+
+# Create Right route for turning the camera right
+@app.route('/right', methods=["POST", "GET"])
+def right():
+    if request.method =="POST":
+        print("RIGHT REQUEST IS SUCCESFUL")
+        # os.system("python SSH_right.py")
+        # retrieve the parameter values from session
+        hst = session.get('hst', '10.0.0.83')
+        usr = session.get('usr', 'gurkaran')
+        pwd = session.get('pwd', 'gurkaran')
+        # call connectRight with the retrieved values
+        SSH_right.connectRight(hst, usr, pwd)
+        return ('', 204)
+
+# Create History route to store history   
+@app.route('/history')
+def history():
+    with open(csv_dir, 'r') as f:
+        reader = csv.DictReader(f)
+        data = list(reader)
+    video_files = [f for f in os.listdir(videos_dir) if f.endswith('.mp4')]
+    return render_template('history.html', data=data, video_files=video_files)
+
+# Allow downloading of captured videos
+@app.route('/video/<filename>')
+def download_video(filename):
+    return send_from_directory(videos_dir, filename, as_attachment=True)
+
+# Allow downloads of history, training data and labels
+@app.route('/download_history')
+def download_history():
+    return send_file(csv_dir, as_attachment=True)
+
+@app.route('/download_training')
+def download_training():
+    return send_file(yml_dir, as_attachment=True)
+
+@app.route('/download_labels')
+def download_labels():
+    return send_file(pickle_dir, as_attachment=True)
+
+# Download all images/ Training Data in a zip
+@app.route('/download_images')
+def download_images():
+    # Create a temporary directory to store the zipped images
+    tmp_dir = os.path.join(BASE_DIR, "tmp")
+    if not os.path.exists(tmp_dir):
+        os.makedirs(tmp_dir)
+
+    # Zip each user folder in the images directory
+    for user_dir in os.listdir(images_dir):
+        user_path = os.path.join(images_dir, user_dir)
+        if os.path.isdir(user_path):
+            zip_path = os.path.join(tmp_dir, "{}.zip".format(user_dir))
+            shutil.make_archive(zip_path[:-4], 'zip', user_path)
+
+    # Zip all the user folders together
+    zip_path = os.path.join(tmp_dir, "images.zip")
+    with zipfile.ZipFile(zip_path, "w") as zip_file:
+        for user_dir in os.listdir(images_dir):
+            user_path = os.path.join(images_dir, user_dir)
+            if os.path.isdir(user_path):
+                for file_name in os.listdir(user_path):
+                    file_path = os.path.join(user_path, file_name)
+                    zip_file.write(file_path, os.path.join(user_dir, file_name))
+
+    # Download the zipped images directory
+    return send_file(zip_path, as_attachment=True)
+
+# Call main and run app
+if __name__=="__main__":
+    app.run(debug=True)
+
---
+++ backend/capture.py
@@ -0,0 +1,72 @@
+import cv2
+import os
+import csv
+from datetime import datetime
+
+
+def capture(name, n):
+    # Create face detector and recognizer
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+    recognizer = cv2.face.EigenFaceRecognizer_create()
+
+    name = str(name)
+
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    img_dir = os.path.join(BASE_DIR, "images")
+    # Create directory for user's face images
+    user_dir = os.path.join(str(img_dir), name)
+    if not os.path.exists(user_dir):
+        os.makedirs(user_dir)
+
+    # Create CSV file to record image filenames and capture times
+    csv_path = os.path.join(BASE_DIR, "capture_history.csv")
+    csv_header = ["filename", "timestamp", "name", "num_images"]
+    if not os.path.exists(csv_path):
+        with open(csv_path, mode='w') as file:
+            csv_writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            csv_writer.writerow(csv_header)
+    
+
+    # Set up video capture device
+    cap = cv2.VideoCapture(0)
+
+    # Capture n face images
+    for i in range(int(n)):
+        # Capture frame from video stream
+        ret, frame = cap.read()
+
+        # Convert frame to grayscale
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Detect faces in the frame
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+        # Display image with face detection overlay
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+        # Display captured frame
+        cv2.imshow("Capture", frame)
+
+        # Save face image if only one face is detected
+        if len(faces) == 1:
+            face = cv2.resize(gray[y:y+h, x:x+w], (200, 200))  # Crop and resize face
+            filename = os.path.join(user_dir, "{}.jpg".format(i))
+            cv2.imwrite(filename, face)
+            print("Saved face image: {}".format(filename))
+            # Record filename and capture time in CSV file
+            with open(csv_path, mode='a') as file:
+                csv_writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                csv_writer.writerow([filename, datetime.now().strftime('%Y-%m-%d %H:%M:%S'), name, n])
+
+        # Wait for user to press 'q' key to quit
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+   
+
+    # Release video capture device and close windows
+    cap.release()
+    cv2.destroyAllWindows()
+
+    return "capture complete"
